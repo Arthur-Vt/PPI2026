1b3d65bcfdfb223e46325c692bc39ed52ad9ad91
Arthur V. <arthur.vitor@escolar.ifrn.edu.br>
2026-05-27 18:58:50 +0000

    Aula 27/05 - Conclusão do Desafio 1

--- ppi26.js
+++ ppi26.js
@@ -255,39 +255,3 @@ console.log(pessoa2);
 const { nome, idade } = pessoa2;
 console.log(nome, idade);
 
-// Construtor - Protótipo de Objeto
-function Product(name, sto, ram, price, qty, category) {
-  this.name = name;
-  this.sto = sto;
-  this.ram = ram;
-  this.price = price;
-  this.qty = qty;
-  this.category = category;
-}
-
-const p1 = new Product("IPhone", 256, 16, 5000, 10, "Celular");
-const p2 = new Product("IPad", 512, 16, 8000, 20, "Tablet");
-const p3 = new Product("Macbook", 1024, 24, 15000, 30, "Notebook");
-const p4 = new Product("IPhone Pro Max", 256, 16, 7000, 15, "Celular");
-const p5 = new Product("IPad Pro", 512, 16, 10000, 25, "Tablet");
-const p6 = new Product("Macbook Pro", 1024, 24, 25000, 35, "Notebook");
-
-const e = [{ ...p1 }, { ...p2 }, { ...p3 }, { ...p4 }, { ...p5 }, { ...p6 }];
-console.log(e);
-
-/* Desafio 1
-
-  a) Valor total de todos os produtos em estoque
-  b) Ordene os produtos por preço (crescente/descrescente)
-  c) Ordene os produtos por nome (crescente/descrescente)
-  d) Filtre produtos de acordo por uma categoria
-
-*/
-
-// let valorTotal = 0;
-// const { price, qty} = valorTotal;
-// console.log(price * qty)
-const valorTotal = e
-  .reduce((valor, p) => valor + p.price * p.qty, 0)
-  .toFixed(2);
-console.log(`O valor total é R$ ${valorTotal}.`);
